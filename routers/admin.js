5f6277d1e389b030b38d3c6bb7ba26cf62782fac
didiernikubwimana <57959937+didiernikubwimana@users.noreply.github.com>
2020-03-13 13:58:00 -0500

    added queries for admin routers

--- routers/admin.js
+++ routers/admin.js
@@ -8,4 +8,36 @@ router.get("/alluser",async (req,res)=>{
     dbCollection.find({}).toArray((err,array)=>res.json(array));
 })});
 
-module.exports = router
+router.post('/activate/:email', async (req,res)=>{
+    await db.initialize(dbName, collectionName, function (dbCollection) {
+        dbCollection.updateOne({email:req.params.email},
+            {
+     $set:{status:'active'}
+        });
+    })
+    
+});
+
+
+router.post('/deactivate/:email', async (req,res)=>{
+    await db.initialize(dbName, collectionName, function (dbCollection) {
+        dbCollection.updateOne({email:req.params.email},
+            {
+     $set:{status:'deactive'}
+        });
+    })
+    
+});
+
+
+router.post('/role/:email', async (req,res)=>{
+    await db.initialize(dbName, collectionName, function (dbCollection) {
+        dbCollection.updateOne({email:req.params.email},
+            {
+     $set:{role:req.body.role}
+        });
+    })
+    
+});
+module.exports=router;
+
