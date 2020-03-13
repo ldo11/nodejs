06bcdbddce515668be75bc2171b6f503246be2c2
dluat <doluat11@gmail.com>
2020-03-13 14:51:48 -0500

    body parser

--- server.js
+++ server.js
@@ -7,21 +7,25 @@ const userRouter = require("./routers/user")
 require('dotenv').config();
 const port = process.env.PORT
 require('./db/db')
+const bodyParser = require('body-parser')
 
 
 
-app.use('/admin', admin)
-app.use('/profile', profile)
-app.use('/users', userRouter)
 app.use(express.json())
 // app.use(userRouter)
-
+app.use(express.urlencoded({ extended: true }))
 app.use((err,req,res,next)=>{
     res.status(err.status ||500);
     res.render('erro',{
         message: err.message, error:{}
     });
 })
+
+
+app.use('/admin', admin)
+app.use('/profile', profile)
+app.use('/users', userRouter)
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
