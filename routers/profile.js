947a5cd440c4fb2f3dc4cebfd7214eb0bbbc19ab
ntpdlp <ntpdlp@gmail.com>
2020-03-13 16:30:38 -0500

    api post a profile

--- routers/profile.js
+++ routers/profile.js
@@ -11,4 +11,20 @@ router.get("/:email",async (req,res)=>{
     })
 });
 
+router.post("/",async (req,res)=>{
+    await db.initialize(dbName, collectionName, function (dbCollection) {
+        const name = req.body.name;
+        const email = req.body.email;
+        const password = req.body.password;
+        const avatar = req.body.avatar;
+        const phone = req.body.phone;
+        const role = req.body.role;
+        const status = req.body.status;
+
+        dbCollection.insertOne({"email":email, "name":name,"password":password, "avatar":avatar,"phone":phone,"role":role,"status":status});
+        res.json('inserted...');
+    })
+});
+
+
 module.exports = router;
