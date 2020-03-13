e2287c6c2f2a371eba8291132366c06dcd258703
ntpdlp <ntpdlp@gmail.com>
2020-03-13 10:24:35 -0500

    modify Router

--- profile.js
+++ profile.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const router = express.router();
+const router = express.Router();
 const db = require("./dbconfig");
 const dbName = "mwa";
 const collectionName = "users";
 
-router.get("/alluser",async (req,res)=>{
+router.get("/:email",async (req,res)=>{
     await db.initialize(dbName, collectionName, function (dbCollection) {
         dbCollection.find({}).toArray((err,array)=>res.json(array));
     })});
