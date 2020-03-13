8a70664725c384afbd4710dbfb494246f6715514
didiernikubwimana <57959937+didiernikubwimana@users.noreply.github.com>
2020-03-13 15:28:28 -0500

    ADDING RETURN TO THE API

--- routers/admin.js
+++ routers/admin.js
@@ -8,34 +8,37 @@ router.get("/alluser",async (req,res)=>{
     dbCollection.find({}).toArray((err,array)=>res.json(array));
 })});
 
-router.post('/activate/:email', async (req,res)=>{
+router.post('/activate', async (req,res)=>{
     await db.initialize(dbName, collectionName, function (dbCollection) {
-        dbCollection.updateOne({email:req.params.email},
+        dbCollection.updateOne({email:req.body.email},
             {
      $set:{status:'active'}
         });
+        res.json("{activate_sucess}")
     })
     
 });
 
 
-router.post('/deactivate/:email', async (req,res)=>{
+router.post('/deactivate', async (req,res)=>{
     await db.initialize(dbName, collectionName, function (dbCollection) {
-        dbCollection.updateOne({email:req.params.email},
+        dbCollection.updateOne({email:req.body.email},
             {
      $set:{status:'deactive'}
         });
+        res.json("{DEactivate_sucess}")
     })
     
 });
 
 
-router.post('/role/:email', async (req,res)=>{
+router.post('/role', async (req,res)=>{
     await db.initialize(dbName, collectionName, function (dbCollection) {
-        dbCollection.updateOne({email:req.params.email},
+        dbCollection.updateOne({email:req.body.email},
             {
      $set:{role:req.body.role}
         });
+        res.json("{ROLE_CHANGE_sucess}")
     })
     
 });
