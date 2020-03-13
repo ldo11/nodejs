f2a52036796482a13598d5d70939f3662de041da
dluat <doluat11@gmail.com>
2020-03-12 20:14:47 -0500

    first api

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express")
 const app = express();
 const admin = require("./admin")
 
-
+app.post('/',(req, res)=>{
+    console.log("first api");
+})
 
 
 app.use("/admin", admin)
