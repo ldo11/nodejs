2a186dcde79055bc494cc636b5fa6a970459e5cb
dluat <doluat11@gmail.com>
2020-03-13 16:27:54 -0500

    mongoose update

---
+++ models/project.js
@@ -0,0 +1,73 @@
+const mongoose = require('mongoose')
+
+const projectSchema = mongoose.Schema({
+    name: {
+        type: String,
+        unique: true,
+        trim: true
+    },
+    testers:[{type: String,
+        trim: true}],
+    TCes: [{
+        tc_name:{
+            type: String,
+            unique: true,
+            lowercase: true,
+        },
+        steps:[{
+            action:{
+                type: String,
+                trim: true
+            },
+            expected:{
+                type: String,
+                trim: true
+            }
+        }],
+        status:{
+            type: String,
+            trim: true
+        },
+        creator:{
+            type: String,
+            trim: true
+        },
+        reviewer:{
+            type: String,
+            trim: true
+        }
+    }],
+    executions:[{
+        tcname:{
+            type: String,
+            trim: true
+        },
+        tester:{
+            type: String,
+            trim: true
+        },
+        build_number:{
+            type: String,
+            trim: true
+        },
+        results:[{
+            result:{
+                type: String,
+                trim: true
+            },
+            comment:{
+                type: String,
+                trim: true
+            },
+            evidents:[{
+                img:{
+                    type: String,
+                    trim: true
+                }
+            }]
+        }]
+    }]
+});
+const project = mongoose.model('projects', projectSchema)
+
+module.exports = project
