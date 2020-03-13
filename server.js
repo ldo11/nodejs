9aa01f99470cc69c9ca49e1c74c0d571ae92b829
ntpdlp <ntpdlp@gmail.com>
2020-03-13 10:06:16 -0500

    add profile into server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const app = express();
 const admin = require("./admin")
+const profile = require("./profile")
 
 
 
 
 app.use("/admin", admin)
+app.use("/profile", profile)
 app.listen(3000);
