b1d3fae678577142f973c17644b27fc2dd4f1c4e
ntpdlp <ntpdlp@gmail.com>
2020-03-14 06:27:51 -0500

    add semicolon

--- server.js
+++ server.js
@@ -4,25 +4,25 @@ const auth = require("./middleware/auth")
 const adminRouter = require("./routers/admin")
 //const profileRouter = require("./routers/profile")
 const userRouter = require("./routers/user")
-const projectRouter = require("./routers/project")
+// const projectRouter = require("./routers/project")
 //const executionRouter = require("./routers/execution")
 require('dotenv').config();
-const port = process.env.PORT
-require('./db/db')
-const bodyParser = require('body-parser')
+const port = process.env.PORT;
+require('./db/db');
+const bodyParser = require('body-parser');
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/admin', adminRouter)
+app.use('/admin', adminRouter);
 //app.use('/profile', profileRouter)
-app.use('/users', userRouter)
-app.use('/projects', projectRouter)
+app.use('/users', userRouter);
+// app.use('/projects', projectRouter)
 //app.use('/execution', executionRouter)
 
 app.use((err,req,res,next)=>{
     res.status(err.status ||500);
-    res.render('erro',{
+    res.render('error',{
         message: err.message, error:{}
     });
 })
@@ -31,6 +31,6 @@ app.use((err,req,res,next)=>{
 //     res.json("hahahaha")
 // })
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+    console.log(`Server running on port ${port}`);
 });
 
