82f00b57d95c3ed6e46e57601c99c83eccd07a02
didiernikubwimana <57959937+didiernikubwimana@users.noreply.github.com>
2020-03-13 22:46:51 -0500

    For Luat review on Project

--- routers/project.js
+++ routers/project.js
@@ -1,44 +1,27 @@
 const router = require('express').Router()
 const Project = require("../models/project.js")
-const db = require("../dbconfig");
+//const db = require("../dbconfig");
 const dbName = "mwa"; //homework07.lectures
 const collectionName = "projects";
 
-
-//to insert project with project name in body
-router.post('/', async (req,res)=>{
-    await db.initialize(dbName, collectionName, function (dbCollection) {
-        dbCollection.insert({"name":req.body.name});
-        res.json("{Project created }")
-    })
-    
-});
-
-//add testcase to project
-
 router.post('/addtestcases', async (req,res)=>{
-    await db.initialize(dbName, collectionName, function (dbCollection) {
-        dbCollection.updateOne({name:req.body.name},
-            {
-     $push:{TCes:{name:req.body.testname,reviewer:req.body.reviewer}}
-        });
-        res.json("{testcase added}")
-    })
-    
+    try{
+        await Project.update({name:req.body.name}, {$push:{TCes:{name:req.body.testname}}})
+        res.json("{Test case added }")
+    }catch (error) {
+        res.status(400).send(error)
+    }
 });
-
-//list all test cases for the  given project
 
 router.get("/alltestcases",async (req,res)=>{
-    await db.initialize(dbName, collectionName, function (dbCollection) {
-    dbCollection.find({name:req.body.name}).toArray((err,array)=>res.json(array));
-})});
-
-//to list all projects
-router.get("/all",async (req,res)=>{
-    await db.initialize(dbName, collectionName, function (dbCollection) {
-    dbCollection.find({}).toArray((err,array)=>res.json(array));
-})});
+    try{
+        await Project.find({name:req.body.name},(error,project)=>{
+            res.json({project})
+        });
+    }catch (error) {
+        res.status(400).send(error)
+    }
+});
 
 
 
