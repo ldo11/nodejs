e3083d117f634ffae84257ff86396466d589e87e
ntpdlp <ntpdlp@gmail.com>
2020-03-14 07:18:11 -0500

    init for execution

--- routers/execution.js
+++ routers/execution.js
@@ -2,6 +2,15 @@ const router = require('express').Router();
 const Project = require('../models/Mproject');
 const collectionName = "projects";
 
-
+Project.get("/allproject",async (req,res)=>{
+    try{
+        Project.find({} , function(err,projects){
+            console.log(projects);
+            res.join(projects);
+        });
+    }catch(error){
+        res.status(400).send(error);
+    }
+});
 
 module.exports=router;
