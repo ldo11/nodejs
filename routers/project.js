d69dd4770092a56638bfd4a20e6b84098ec3b2f1
didiernikubwimana <57959937+didiernikubwimana@users.noreply.github.com>
2020-03-14 10:16:58 -0500

    FOR SUPPORT ABOUT PROJECT POSTING APIs

--- routers/project.js
+++ routers/project.js
@@ -1,17 +1,33 @@
 const router = require('express').Router()
-const Project = require("../models/project.js")
+const Project = require("../models/Mproject.js")
 //const db = require("../dbconfig");
 const dbName = "mwa"; //homework07.lectures
 const collectionName = "projects";
 
+
+
+        //to insert project with project name in body
+router.post('/', async (req,res)=>{
+    try{
+        await MProject.create({name:req.body.name})
+        res.json("{Project created! }")
+    }catch (error) {
+        res.status(400).send(error)
+    }
+    
+});
+
+//add testcase to project
+
 router.post('/addtestcases', async (req,res)=>{
     try{
-        await Project.update({name:req.body.name}, {$push:{TCes:{name:req.body.testname}}})
+        await MProject.updateOne({name:req.body.name}, {$push:{"TCes":{tc_name:req.body.tcname}}})
         res.json("{Test case added }")
     }catch (error) {
         res.status(400).send(error)
     }
 });
+
 
 router.get("/alltestcases",async (req,res)=>{
     try{
