75561d5f365ab6d2bed7dcb53ede9a8e23508d62
ntpdlp <ntpdlp@gmail.com>
2020-03-14 06:42:11 -0500

    update post profile

--- routers/profile.js
+++ routers/profile.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const User = require('../models/Muser')
+const User = require('../models/Muser');
 router.get("/:email",async (req,res)=>{
     try{
         await User.findOne({email:req.body.email},(error,user)=>{
-            res.json({user})
+            res.json({user});
         });
     }catch (error) {
-        res.status(400).send(error)
+        res.status(400).send(error);
     }
 });
 
 router.post("/",async (req,res)=>{
     try{
         await User.findOneAndUpdate({email:req.body.email},{name:req.body.name, phone:req.body.phone, avatar:req.body.avatar});
-        res.json("Profile update successful");
+        res.json("Profile update successfully");
     }catch (error) {
-        res.status(400).send(error)
+        res.status(400).send(error);
     }
 });
 
